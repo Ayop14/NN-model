c439ae9fd9dae56380549e1e6d9d9bd32592e98b
Ayop14 <betollopis15@gmail.com>
2022-08-18 13:41:48 +0200

    overflow in cost function solved

--- cost_functions.py
+++ cost_functions.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def log_reg_cost(pred, tr_lab):
-    cost = np.sum(tr_lab * np.log(pred) + (1 - tr_lab) * np.log(1-pred))
+    cost = np.sum(tr_lab * np.log(pred + 1e-6) + (1 - tr_lab) * np.log(1-pred + 1e-6))
     return  -1/tr_lab.shape[1] * cost
 
 def log_reg_cost_grad(A, Y):
